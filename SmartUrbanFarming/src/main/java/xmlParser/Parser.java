18cc69ab3b01cfcb27f4cd58d95f1b905f3ee8d9
Beate Scheibel <beate.scheibel@gmail.com>
2018-11-01 11:22:59 +0100

    xml parser finished

--- SmartUrbanFarming/src/main/java/xmlParser/Parser.java
+++ SmartUrbanFarming/src/main/java/xmlParser/Parser.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,7 +24,7 @@ public class Parser {
 	public static void main(String[] args) {
 
 		try {
-			FileInputStream file = new FileInputStream(new File("/Users/Beate/Desktop/greenhouse.xml"));
+			FileInputStream file = new FileInputStream(new File("greenhouse.xml"));
 				
 			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
 			
@@ -33,29 +34,107 @@ public class Parser {
 
 			XPath xPath =  XPathFactory.newInstance().newXPath();
 			
-			ArrayList<String> crops = new ArrayList<String>();
+			ArrayList<String> location = new ArrayList<String>();
 			
-			XPathExpression expr= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@class=\"Crop\"]/@name");
-			NodeList nl = (NodeList) expr.evaluate(xmlDocument, XPathConstants.NODESET);
-			for (int i = 0; i < nl.getLength(); i++) {
-			    Node node = nl.item(i);
-			    crops.add(node.getTextContent());		
+			
+			//Get all location inside the model
+			XPathExpression expr5= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@class=\"Location\"]/@name");
+			NodeList n1 = (NodeList) expr5.evaluate(xmlDocument, XPathConstants.NODESET);
+			for (int i = 0; i < n1.getLength(); i++) {
+			    Node node = n1.item(i);
+			    location.add(node.getTextContent());		
+			    System.out.println("Location name: " + node.getTextContent());
 			}
-
-			for(int i=0; i<crops.size(); i++) {
-			String name = crops.get(i);
-			System.out.println(name);
 			
-			XPathExpression expr1= xPath.compile("/ADOXML/MODELS/MODEL/CONNECTOR[@class=\"Requires\"]/FROM[@instance=\""+name+"\"]/../TO/@instance");
-			NodeList nl1 = (NodeList) expr1.evaluate(xmlDocument, XPathConstants.NODESET);
-				for (int j = 0; j < nl1.getLength(); j++) {
-					Node node = nl1.item(j);
-					String needs = node.getTextContent().split("-")[0];
-					System.out.println(needs);
 						
-					XPathExpression expr2 =  xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name=\""+node.getTextContent()+"\"]/ATTRIBUTE[@name=\"Amount\"]");
-					String evalu = (String) expr2.evaluate(xmlDocument, XPathConstants.STRING);
-					System.out.println(evalu);
+			//For all location get the attributes (lat,long,direction and city)
+			for(int i=0; i<location.size(); i++) {
+				String name = location.get(i);
+				XPathExpression expr6= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name +"']/ATTRIBUTE[@name=\"Latitude\"]");
+				NodeList nodel = (NodeList) expr6.evaluate(xmlDocument, XPathConstants.NODESET);
+				for (int j = 0; j < nodel.getLength(); j++) {
+				    Node node = nodel.item(j);	
+				    System.out.println("Latitude: " + node.getTextContent());
+				}
+				XPathExpression expr7= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name +"']/ATTRIBUTE[@name=\"Longitude\"]");
+				NodeList n2 = (NodeList) expr7.evaluate(xmlDocument, XPathConstants.NODESET);
+				for (int j = 0; j < n2.getLength(); j++) {
+				    Node node = n2.item(j);
+				    System.out.println("Longitude: " + node.getTextContent());
+				}
+				XPathExpression expr8= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name +"']/ATTRIBUTE[@name=\"Direction\"]");
+				NodeList n3 = (NodeList) expr8.evaluate(xmlDocument, XPathConstants.NODESET);
+				for (int j = 0; j < n3.getLength(); j++) {
+				    Node node = n3.item(j);	
+				    System.out.println("Direction: " + node.getTextContent());
+				}
+				XPathExpression expr9= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name +"']/ATTRIBUTE[@name=\"City\"]");
+				NodeList n4 = (NodeList) expr9.evaluate(xmlDocument, XPathConstants.NODESET);
+				for (int j = 0; j < n4.getLength(); j++) {
+				    Node node = n4.item(j);	
+				    System.out.println("City: " + node.getTextContent());
+				}
+				
+		
+				//Get all farming modules at this location
+				ArrayList<String> module = new ArrayList<String>();
+				
+				XPathExpression expr10= xPath.compile("/ADOXML/MODELS/MODEL/CONNECTOR[@class=\"In\"]/FROM[@instance=\""+ name +"\"]/../TO/@instance");
+				NodeList n12 = (NodeList) expr10.evaluate(xmlDocument, XPathConstants.NODESET);
+				for (int k = 0; k < n12.getLength(); k++) {
+				    Node node = n12.item(k);
+				    module.add(node.getTextContent());		
+				    System.out.println("Farming Module name: " + node.getTextContent());
+				}
+				for(int l=0; l<module.size(); l++) {
+					String name1 = module.get(l);
+					XPathExpression expr16= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name1 +"']/ATTRIBUTE[@name=\"Length\"]");
+					NodeList n5 = (NodeList) expr16.evaluate(xmlDocument, XPathConstants.NODESET);
+					for (int j = 0; j < n5.getLength(); j++) {
+					    Node node = n5.item(j);
+					    System.out.println("Lenght: " + node.getTextContent());
+					}
+					XPathExpression expr17= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name1 +"']/ATTRIBUTE[@name=\"Height\"]");
+					NodeList n6 = (NodeList) expr17.evaluate(xmlDocument, XPathConstants.NODESET);
+					for (int j = 0; j < n6.getLength(); j++) {
+					    Node node = n6.item(j);
+					    System.out.println("Height: " + node.getTextContent());
+					}
+					XPathExpression expr18= xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name='" + name1 +"']/ATTRIBUTE[@name=\"Width\"]");
+					NodeList n7 = (NodeList) expr18.evaluate(xmlDocument, XPathConstants.NODESET);
+					for (int j = 0; j < n7.getLength(); j++) {
+					    Node node = n7.item(j);
+					    System.out.println("Width: " + node.getTextContent());
+					}
+					
+					
+					//Get all crops in this farming module
+					ArrayList<String> crops = new ArrayList<String>();
+					XPathExpression expr19= xPath.compile("/ADOXML/MODELS/MODEL/CONNECTOR[@class=\"In\"]/TO[@instance=\""+ name1 +"\"]/../FROM/@instance");
+					NodeList n19 = (NodeList) expr19.evaluate(xmlDocument, XPathConstants.NODESET);
+					for (int k = 0; k < n19.getLength(); k++) {
+					    Node node = n19.item(k);
+					    if((!Objects.equals(name,node.getTextContent()))) {
+						    crops.add(node.getTextContent());		
+						    System.out.println("Crop name: " + node.getTextContent());
+					    }
+					}
+					for(int m=0; m<crops.size(); m++) {
+						String name2 = crops.get(m);
+					
+						XPathExpression expr1= xPath.compile("/ADOXML/MODELS/MODEL/CONNECTOR[@class=\"Requires\"]/FROM[@instance=\""+name2+"\"]/../TO/@instance");
+						NodeList nl1 = (NodeList) expr1.evaluate(xmlDocument, XPathConstants.NODESET);
+							for (int j = 0; j < nl1.getLength(); j++) {
+								Node node = nl1.item(j);
+								String needs = node.getTextContent().split("-")[0];
+								System.out.print(name2 + " needs " + needs);
+									
+								XPathExpression expr2 =  xPath.compile("/ADOXML/MODELS/MODEL/INSTANCE[@name=\""+node.getTextContent()+"\"]/ATTRIBUTE[@name=\"Amount\"]");
+								String evalu = (String) expr2.evaluate(xmlDocument, XPathConstants.STRING);
+								System.out.println(": " + evalu);
+						}
+							
+					}	
 				}
 			}
 			
