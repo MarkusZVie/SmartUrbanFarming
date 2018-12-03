122b49c496478ebb4871b96c461b0fa411dd6bc6
Beate Scheibel <beate.scheibel@gmail.com>
2018-12-03 08:21:02 +0100

    write resultset to csv finished

--- SmartUrbanFarming/src/main/java/model_parser/dbToCsv.java
+++ SmartUrbanFarming/src/main/java/model_parser/dbToCsv.java
@@ -1,23 +1,34 @@
 package model_parser;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 
+
 public class dbToCsv {
 	static Statement stmt=null;
 	static ResultSet rs = null;
-	 
-    public static void readData(){
+	static String record = null;
+
+    public static void readData() throws IOException, SQLException{
+    		FileWriter f1 = new FileWriter("hallo.csv");
+
         try{
     			String dbUrl = "jdbc:derby:farming;create=false";
     			Connection conn = DriverManager.getConnection(dbUrl);
             String query="SELECT * FROM sensordata";
              stmt=conn.createStatement();
              rs=stmt.executeQuery(query);
+             ResultSetMetaData metaData = rs.getMetaData();
+             int columns = metaData.getColumnCount();
+             
+             
              while (rs.next()) {
                  String s = rs.getString("farm_name");
                  Timestamp n = rs.getTimestamp("time_");
@@ -28,22 +39,24 @@ public class dbToCsv {
                  String name = rs.getString(3);
                  System.out.println(name);
                  System.out.println(s + "   " + n + "   " + h + "   " + t + "   " + l + "   " + hy);
+                 for (int i = 1; i <= columns; i++) 
+                 {
+                  record = rs.getString(i);
+                  f1.append(rs.getString(i));
+                  f1.append(',');
+                 }
+                 f1.append('\n');
+             	}
+             } 
+             finally 
+             {
+                 f1.flush();
+                 rs.close();
+                 stmt.close();
+                 f1.close();
              }
-        }catch(SQLException s){
-            s.printStackTrace();
-        }
     }
-    
-   /* ResultSet resultSet = getData();
-    ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-    int columnCount = resultSetMetaData.getColumnCount();
+}    
 
-    while (resultSet.next()) {
-        Object[] values = new Object[columnCount];
-        for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
-            values[i - 1] = resultSet.getObject(i);
-        }
-        String []model.addRow(values);
-    }*/
-	
-}
+    
+  
--- SmartUrbanFarming/src/main/java/model_parser/test_db.java
+++ SmartUrbanFarming/src/main/java/model_parser/test_db.java
@@ -1,8 +1,11 @@
 package model_parser;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class test_db{
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException, SQLException{
 		String name = "";
 		System.out.println(name);
 		DB_connection.dbSensors(name, 4748+"", 49494+"", 4949+"", 4944+"");
